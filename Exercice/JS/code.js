f1baa7a21b103776e60dc7495806600f28bbb0cb
KevinVaucherCPNV <47849513+KevinVaucher@users.noreply.github.com>
2019-10-07 08:25:22 +0200

    wip

--- Exercice/JS/code.js
+++ Exercice/JS/code.js
@@ -1,29 +1,32 @@
 document.addEventListener("DOMContentLoaded", init)
 
-function Blue() {
+
+function Blue() { // Fonction exercice 1 - Tracer le texte + background = bleu
     div1.style.backgroundColor = 'lightblue';
     div1.style.textDecoration = 'line-through';
 }
 
-function Clean() {
+function Clean() { // Fonction exercice 2 - Annuler exercice 1
     div1.style.backgroundColor = 'white';
     div1.style.textDecoration = 'none';
 }
 
-function Arial() {
+function Arial() { // Fonction exercice 3 (ajouter la police)
     div3.classList.add('classdiv3');
 }
 
-function RemoveArial() {
+function RemoveArial() { // Fonction exercice 3 (retirer la police)
     div3.classList.remove('classdiv3');
 }
-function CheckboxCheck(){
-    if(div4chk1.checked == true && div4chk2.checked == true){
-        div4.classList.add('classdiv4');
+
+function CheckboxCheck() { // Fonction exercice 4 - Changer le texte d'une div + la couleur
+    if (div4chk1.checked && div4chk2.checked) {
+        div4.style.backgroundColor="Green"
+        div4.style.color="White"
+        div4.style.border="solid 0px black"
+        document.getElementById("div4").innerHTML ="YES";
     }
-    else{
-        div4.classList.remove('classdiv4');
-    }
+
 }
 
 function init() {
