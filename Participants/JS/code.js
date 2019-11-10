9c9ea5c9de3b3c1960fcf2348237669269123f67
KevinVaucherCPNV <47849513+KevinVaucher@users.noreply.github.com>
2019-11-10 19:20:52 +0100

    Done Participants

--- Participants/JS/code.js
+++ Participants/JS/code.js
@@ -5,7 +5,8 @@ Description : Page internet pour le module ICT-120
 Version : 1.0
  */
 
-document.addEventListener("DOMContentLoaded", init) // Permet d'appeler la fonction init dès l'affichage de la page
+// Permet d'appeler la fonction init dès l'affichage de la page
+document.addEventListener("DOMContentLoaded", init)
 
 // On initialise des "écoutes" d'évènement dès l'affichage de la page
 function init() {
@@ -21,42 +22,41 @@ function fnAlert() {
     alert("Désolé, cette partie du site n'est pas encore disponible !");
 }
 
-// Permet d'ajouter une ligne au tableau
+// Fonction pour ajouter une ligne au tableau
 function addLine() {
-    // Fonction pour ajouter une ligne au tableau
-    newtr = document.createElement("tr") // On crée un élément "tr"
-    newtd1 = document.createElement("td") // On crée un élément "td1"
-    newtd1.innerText = (addPrenom.value) // On applique la valeur de ce que l'utilisateur a entré pour la td1 (colonne "prénom")
-    newtd2 = document.createElement("td") // On crée un élément "td1"
-    newtd2.innerText = (addNom.value) // On applique la valeur de ce que l'utilisateur a entré pour la td1 (colonne "prénom")
-    newtd3 = document.createElement("td") // On crée un élément "td1"
-    newtd3.innerText = (addClasse.value) // On applique la valeur de ce que l'utilisateur a entré pour la td1 (colonne "prénom")
-    linkmail = document.createElement("a") // On crée un élément "linkmail" pour transformer la valeur de la colonne "E-mail" en mailto
-    linkmail.text = addEmail.value // On applique la valeur de ce que l'utilisateur a entré pour la td1 (colonne "prénom")
-    linkmail.href = "mailto:" + addEmail.value // On indique que la valeur de la colonne "E-mail" est un e-mail
-    newtd4 = document.createElement("td") // On crée un élément "td1"
-    newtd4.appendChild(linkmail)
-    newtd5 = document.createElement("td") // On crée un élément "td1"
-    newbtn = document.createElement("div") // On crée un élément "newbtn" qui sera pour supprimer la ligne
-    newtd5.appendChild(newbtn)
-    newbtn.classList = "btn btn-danger" // On applique la classe bootstrap pour le bouton supprimer
-    newbtn.innerText = "Supprimer" // On indique la valeur du texte du bouton supprimer
+            newtr = document.createElement("tr") // On crée un élément "tr"
+            newtd1 = document.createElement("td") // On crée un élément "td1"
+            newtd1.innerText = (addPrenom.value) // On applique la valeur de ce que l'utilisateur a entré pour la td1 (colonne "prénom")
+            newtd2 = document.createElement("td") // On crée un élément "td1"
+            newtd2.innerText = (addNom.value) // On applique la valeur de ce que l'utilisateur a entré pour la td1 (colonne "prénom")
+            newtd3 = document.createElement("td") // On crée un élément "td1"
+            newtd3.innerText = (addClasse.value) // On applique la valeur de ce que l'utilisateur a entré pour la td1 (colonne "prénom")
+            linkmail = document.createElement("a") // On crée un élément "linkmail" pour transformer la valeur de la colonne "E-mail" en mailto
+            linkmail.text = addEmail.value // On applique la valeur de ce que l'utilisateur a entré pour la td1 (colonne "prénom")
+            linkmail.href = "mailto:" + addEmail.value // On indique que la valeur de la colonne "E-mail" est un e-mail
+            newtd4 = document.createElement("td") // On crée un élément "td1"
+            newtd4.appendChild(linkmail)
+            newtd5 = document.createElement("td") // On crée un élément "td1"
+            newbtn = document.createElement("div") // On crée un élément "newbtn" qui sera pour supprimer la ligne
+            newtd5.appendChild(newbtn)
+            newbtn.classList = "btn btn-danger" // On applique la classe bootstrap pour le bouton supprimer
+            newbtn.innerText = "Supprimer" // On indique la valeur du texte du bouton supprimer
 
-    newdiv = document.createElement("div")
-    newbtn.addEventListener("click", killLine) // On indique qu'il faut appeler la fonction killLine dès qu'on clique sur le bouton supprimer
-    newtd3.appendChild(newdiv)
-    newtr.appendChild(newtd1)
-    newtr.appendChild(newtd2)
-    newtr.appendChild(newtd3)
-    newtr.appendChild(newtd4)
-    newtr.appendChild(newtd5)
-    tableBody.appendChild(newtr)
+            newdiv = document.createElement("div")
+            newbtn.addEventListener("click", killLine) // On indique qu'il faut appeler la fonction killLine dès qu'on clique sur le bouton supprimer
+            newtd3.appendChild(newdiv)
+            newtr.appendChild(newtd1)
+            newtr.appendChild(newtd2)
+            newtr.appendChild(newtd3)
+            newtr.appendChild(newtd4)
+            newtr.appendChild(newtd5)
+            tableBody.appendChild(newtr)
 
-    // Remise des valeurs des champs textes à 0 une fois que la ligne est ajoutée
-    addPrenom.value = ("")
-    addNom.value = ("")
-    addClasse.value = ("")
-    addEmail.value = ("")
+            // Remise des valeurs des champs textes à 0 une fois que la ligne est ajoutée
+            addPrenom.value = ("");
+            addNom.value = ("");
+            addClasse.value = ("");
+            addEmail.value = ("");
 }
 
 // Fonction pour supprimer une ligne
@@ -68,19 +68,3 @@ function killLine(event) // Le paramètre event représente l'événement qui a 
     table = tr.parentNode   // table est la table dans laquelle la ligne se trouve
     table.removeChild(tr)   // On enlève la ligne de la table
 }
-
-
-/*
-Fonction pour ajouter une colonne
-function addCol()
-{
-    rows = tblBody.children
-    for (i=0; i<rows.length ; i++)
-    {
-        tr = rows[i]
-        newtd = document.createElement('td')
-        newtd.innerText = 'Nouvelle ligne '+i
-        tr.appendChild(newtd)
-    }
-}
-*/
